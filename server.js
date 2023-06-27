0bf431b1e58bf2b5398438674e00d401b6e9e674
Denis <den748@gmail.com>
2023-06-27 17:36:15 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const morgan = require('morgan');
+
+// const PORT = process.env.SERVER_PORT || 3001
+const PORT = process.env.SERVER_PORT 
+const app = express();
+
+app.use(express.json());
+
+app.use(morgan('dev'))
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}` )
+})
